06c67b3c2e1e613adb6336f762e6edbe4a32ac88
rrizaldym <rizky.rizaldy.m@gmail.com>
2021-01-26 22:12:46 +0700

    upload tugas 12

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Tugas12 from './Tugas/Tugas12/App'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tugas12 />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ SanberApp/Tugas/Tugas12/App.js
@@ -0,0 +1,109 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    Image, 
+    TouchableOpacity,
+    FlatList 
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import VideoItem from './components/videoitem';
+import data from './data.json'
+
+export default class App extends Component {
+  render(){
+      return (
+       <View style={styles.container}>
+            <View style={styles.navBar}>
+                <Image source={require('./images/logo.png')} 
+                        style={{width:98, height:22}}/>
+                <View style={styles.rightNav}>
+                    <TouchableOpacity>
+                        <Icon style={styles.navItem} name='search' size={25}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon style={styles.navItem} name='account-circle' size={25}/>
+                    </TouchableOpacity>
+                </View>        
+           </View>
+            
+            <View style={styles.body}>
+                <FlatList 
+                    data={data.items}
+                    renderItem={(video)=> <VideoItem video={video.item} /> } 
+                    keyExtractor={(item)=>item.id}
+                    ItemSeparatorComponent={()=><View style={{height:0.5, backgroundColor:'#e5e5e5'}} /> }
+                />
+            </View>
+
+            <View style={styles.tabBar}>
+                <TouchableOpacity style={styles.tabItem}>
+                    <Icon name="home" size={25} />
+                    <Text style={styles.tabTitle}>Home</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.tabItem}>
+                    <Icon name="whatshot" size={25} />
+                    <Text style={styles.tabTitle}>Trending</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.tabItem}>
+                    <Icon name="subscriptions" size={25} />
+                    <Text style={styles.tabTitle}>Subscriptions</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.tabItem}>
+                    <Icon name="folder" size={25} />
+                    <Text style={styles.tabTitle}>Library</Text>
+                </TouchableOpacity>
+            </View>
+       </View>
+     )
+
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  navBar:{
+    height: 55,
+    backgroundColor: 'white',
+    elevation: 3,
+    paddingHorizontal: 15,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between'
+
+  },
+  rightNav:{
+      flexDirection:'row'
+  },
+  navItem:{
+      marginLeft: 25,
+  },
+  body:{
+    flex:1,
+  },
+  tabBar:{
+    backgroundColor:'white',
+    height:60,
+    borderTopWidth:0.5,
+    borderColor:'#E5E5E5',
+    flexDirection:'row',
+    justifyContent: 'space-around',
+  },
+  tabItem:{
+      alignItems:'center',
+      justifyContent: 'center'
+  },
+  tabTitle:{
+      fontSize:11,
+      color:'#3c3c3c',
+      paddingTop:4,
+  }
+
+});
